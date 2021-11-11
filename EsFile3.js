fb0448b36f01aef066304ef62ccd467eb0626882
Baldan Federico <baldan.federico@studenti.salesianisesto.it>
2021-11-11 10:41:27 +0100

    Function optimization

--- EsFile3.js
+++ EsFile3.js
@@ -1,58 +1,43 @@
 $("#btnplus").click(function(){
-    var input1 = $("#num1");
-    var input2 = $("#num2");
-    if(input1.val() && input2.val()){
-        var n1= parseInt(input1.val());
-        var n2= parseInt(input2.val());
-        var risultato = n1 + n2;
-        var string = 
-        "<tr><td>" + n1 + "</td><td style ='text-allign:center'>+</td><td>" + n2 +"</td><td>"+ risultato +"</td></tr>";
-        $('#table').append(string);
-        input1.val("");
-        input2.val("");
-    }
+calc ("+");
 });
+
 $("#btn-").click(function(){
-    var input1 = $("#num1");
-    var input2 = $("#num2");
-    if(input1.val() && input2.val()){
-        var n1= parseInt(input1.val());
-        var n2= parseInt(input2.val());
-        var risultato = n1 - n2;
-        var string = 
-        "<tr><td>" + n1 + "</td><td style ='text-allign:center'>-</td><td>" + n2 +"</td><td>"+ risultato +"</td></tr>";
-        $('#table').append(string);
-        input1.val("");
-        input2.val("");
-    }
+calc ("-");  
 });
 
 $("#btnper").click(function(){
-    var input1 = $("#num1");
-    var input2 = $("#num2");
-    if(input1.val() && input2.val()){
-        var n1= parseInt(input1.val());
-        var n2= parseInt(input2.val());
-        var risultato = n1 * n2;
-        var string = 
-        "<tr><td>" + n1 + "</td><td style ='text-allign:center'>*</td><td>" + n2 +"</td><td>"+ risultato +"</td></tr>";
-        $('#table').append(string);
-        input1.val("");
-        input2.val("");
-    }
+calc ("*");
 });
 
 $("#btndiviso").click(function(){
-    var input1 = $("#num1");
-    var input2 = $("#num2");
-    if(input1.val() && input2.val()){
-        var n1= parseInt(input1.val());
-        var n2= parseInt(input2.val());
-        var risultato = n1/n2;
-        var string = 
-        "<tr><td>" + n1 + "</td><td style ='text-allign:center'>/</td><td>" + n2 +"</td><td>"+ risultato +"</td></tr>";
+calc ("/");
+});
+
+function calc(segno) {
+var input1 = $("#num1");
+var input2 = $("#num2");
+var n1= parseInt(input1.val());
+var n2= parseInt(input2.val());
+switch (segno){
+case "+":
+    var risultato = n1 + n2;
+    break;
+case "-":
+    var risultato = n1 - n2;
+    break;
+case "*":
+    var risultato = n1 * n2;
+    break;
+case "/":
+    var risultato = n1 / n2;
+    break;
+}
+if(input1.val() && input2.val()){
+ var string = 
+        "<tr><td>" + n1 + "</td><td style ='text-allign:center'>" + segno + "</td><td>" + n2 +"</td><td>"+ risultato +"</td></tr>";
         $('#table').append(string);
         input1.val("");
         input2.val("");
     }
-});
+}
